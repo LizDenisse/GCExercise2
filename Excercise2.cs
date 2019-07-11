3068e1546fba190d1f9b1c78099a393455fe1358
LizHitches <liz.hitches@gmail.com>
2019-07-11 10:16:18 -0400

    GCExercise2

---
+++ Excercise2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GCExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(number);
+            Console.ReadLine();
+        }
+    }
+}
+/*
+ * EXERCISE 2DescriptionPrompt the user to enter a number. 
+ * After the user enters a number, add 1 to the number andoutput it back to the console.
+ * Example
+ * >>Enter a number: <<52>>
+ * 53
+ */
